afe6661f4f6c1727db4921a297e44068c971114f
sbx_user1059 <sbx_user1059@ip-10-11-234-71.eu-west-2.compute.internal>
2018-04-27 16:04:41 +0000

    Fri Apr 27 16:04:41 UTC 2018

--- src/BeFaster.App/Solutions/FizzBuzzSolution.cs
+++ src/BeFaster.App/Solutions/FizzBuzzSolution.cs
@@ -4,14 +4,25 @@ namespace BeFaster.App.Solutions
 {
     public static class FizzBuzzSolution
     {
+        public static bool IsFizz(int number)
+        {
+            return number % 3 == 0 || number.ToString().Contains("3");
+        }
+
+        public static bool IsBuzz(int number)
+        {
+            return number % 5 == 0 || number.ToString().Contains("5");
+        }
+
         public static string FizzBuzz(int number)
         {
+            bool isFizz = IsFizz(number), isBuzz = IsBuzz(number);
             var result = number.ToString();
-            if (number % 3 == 0 && number % 5 == 0)
+            if (isFizz && isBuzz)
                 result = "fizz buzz";
-            else if (number % 3 == 0)
+            else if (isFizz)
                 result = "fizz";
-            else if (number % 5 == 0)
+            else if (isBuzz)
                 result = "buzz";
             return result;
         }
